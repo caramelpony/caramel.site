513892586498aa401498e4fee21702757b77b185
caramel <caramel@caramel.horse>
2024-09-19 16:29:24 -0600

    Updated pfp

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       backgroundImage: {
         'caramel-pattern': "url('/caramel.png')",
+        'caramel-icon': "url('/caramel.jpg')",
         'caramel-background': "url('/Snapshot_421.png')",
         'fin-flag': "url('https://static.wikia.nocookie.net/gender/images/0/08/Fingender.png')",
         'demirom-flag': "url('https://static.wikia.nocookie.net/lgbtqia-sandbox/images/6/6d/Demiromantic_Flag.svg')",
